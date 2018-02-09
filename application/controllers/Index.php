1bfababfcf2b66314205745b32fcfa85a9763f23
Carl <Carl@192.168.0.102>
2018-02-10 00:34:34 +0800

    chating page.

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -16,7 +16,7 @@ class IndexController extends BaseController {
         if ($this->user) {
 
         }
-        $this->assign('user', '');
+        $this->getView()->assign('user', $this->user);
     }
 
 }
